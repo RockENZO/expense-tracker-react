93c967c62e8ca238251f14654242d3c0dbac3f64
RockENZO <gewang2004@gmail.com>
2024-12-09 22:23:51 +1030

    indentation issue

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -28,10 +28,8 @@ export const AddTransaction = () => {
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label htmlFor="amount"
-            >Amount <br />
-            (negative - expense, positive - income)</label
-          >
+          <label htmlFor="amount">Amount <br />
+            (negative for expense, positive for income)</label>
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
